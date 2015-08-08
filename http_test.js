e8901f0c8851a57f8a9a87d2310efa3c9a7295dc
smelrain <840277005@qq.com>
2015-08-08 17:15:45 +0800

    添加测试数据

--- http_test.js
+++ http_test.js
@@ -21,8 +21,56 @@ var datas = {
     },
 
     "/date/list": {
+        "data": [
+            {
+                "date_id": "3",                //约会 item id
+                "head": "lijixni123",          //用户头像
+                "nickname": "smelrain",            //用户昵称
+                "authened": 1,                  //该用户是否认证过 `1`已认证 `2`未认证
+                "uid": "1",                    //用户id
+                "created_at": "1429446315",    //创建时间     时间戳
+                "date_time": "1429456315",     //约的具体时间  时间戳
+                "place": "重邮宾馆",            //地点
+                "title": "来约炮!",             //标题
+                "date_type": "1",              // type
+                "category_id": "1",            //
+                "signature": "今天晚上我请客!",  //个性签名
+                "erolled": 20,                  //多少人已报名
+                "commented": 20,                //多少人已评论
+                "praised": 20,                  //多少人已点赞
+                "gender": 1,                   //性别 "1" 男性  "2"女性
+                "cost_model": "1"              //1AA, 2我请客, 3求请客
+            },
 
+            {
+                "date_id": "4",                //约会 item id
+                "head": "xxxxxxxxxx",          //用户头像
+                "nickname": "lijinxin",         //用户昵称
+                "authened": 0,                  //该用户是否认证过 `1`已认证 `2`未认证
+                "uid": "1",                    //用户id
+                "created_at": "1429446315",    //创建时间     时间戳
+                "date_time": "1429456315",     //约的具体时间  时间戳
+                "place": "重邮宾馆",            //地点
+                "title": "来约炮!",             //标题
+                "date_type": "1",              // type
+                "category_id": "1",            //
+                "signature": "今天晚上我请客!",  //个性签名
+                "erolled": 20,                  //多少人已报名
+                "commented": 20,                //多少人已评论
+                "praised": 20,                  //多少人已点赞
+                "gender": 1,                   //性别 "1" 男性  "2"女性
+                "cost_model": "1"              //1AA, 2我请客, 3求请客
+            }
+        ],
+        "status": 200,
+        "info": "获取成功"
     },
+
+    //"/activity/list": {
+    //    da
+    //    "status": 200,
+    //    "info": "获取成功"
+    //},
 
     "error": {
         "status": 0,
--- src/lib/vms/filter.js
+++ src/lib/vms/filter.js
@@ -5,19 +5,25 @@ define(['avalon', 'vms/navBar'], function(avalon, vmNavBar){
         options: {       //键值为所对应的类型
             date: {
                 type: ['不限', '运动', '聚会', '娱乐', '拼车', '交流', '学习', '棋牌', '旅游', '活动', '其他'],
-                order: ['人气最热', '发布最新'],
+                order: ['', '人气最热', '发布最新'],
                 gender: ['不限', '男', '女'],
                 cost_mode: ['不限', 'AA制', '我请客', '求包养', '无消费'],
                 time: ['不限', '今天', '两天内', '一周内', '只看周末']
             },
             activity: {
                 type: ['不限', '娱乐', '运动', '旅游', '聚会', '交流', '学习', '其他'],
-                orderr: ['离我最近', '发布最新'],
+                order: ['','离我最近', '发布最新'],
                 cost_mode: ['不限', '<30', '30-50', '50-100', '100-200', '200-300', '300-500', '>500'],
                 time: ['不限', '今天', '三天内', '一周内', '只看周末']
             }
         },
-        selected: [],
+        selected: {    //最初加载数据
+            type: 'date',
+            order: 1,
+            date_gender: 0,
+            cost_mode: 0,
+            date_time: 0
+        },
         toggleOpen: function(){
             vm['opened'] = !vm['opened'];
         }
