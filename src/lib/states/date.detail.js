8a7cf64ad78cfe745e8a153da9cb386950ec312a
smelrain <840277005@qq.com>
2015-08-09 17:07:24 +0800

    消费类型

--- src/lib/states/date.detail.js
+++ src/lib/states/date.detail.js
@@ -1,7 +1,15 @@
 
 //约会详细页
-define(['avalon', 'vms/tipBar', 'userCenter', 'vms/main','states/date','mmState', 'dialog'], function(avalon, vmTipBar, userCenter, vmMain){
-    avalon.state('dateDetail', {
+define(
+    [
+        'avalon', 'vms/tipBar', 'userCenter',
+        'vms/main', 'vms/dateDetail', 'request',
+        'mmState',
+        'dialog'
+    ],
+    function(avalon, vmTipBar, userCenter, vmMain, vmDDet, request){
+
+        avalon.state('dateDetail', {
         controller: "main",
         url: "/date/detail/:id",
         templateUrl: "tpl/date/detail.html",
@@ -20,12 +28,12 @@ define(['avalon', 'vms/tipBar', 'userCenter', 'vms/main','states/date','mmState'
             var date_id = this.params.id;
             //todo 没有date_id
 
+            console.log(request);
+
             //获取detail的数据
             request('dateDetail', {date_id: date_id, uid: user.uid, token: user.token})
                 .done(function(res){
-                    vmDetail.data = res.data;
-                    vmDetail['isCollected'] = res.data.collection_status;
-                    vmDetail['isSignedUp'] = res.data.apply_status;
+                    vmDDet.data = res.data;
                     avalon.scan();
                     vmMain['state'] = 'ok';
                 });
--- src/lib/vms/dateDetail.js
+++ src/lib/vms/dateDetail.js
@@ -1,9 +1,12 @@
 define(['avalon'], function(avalon){
     var vm = avalon.define({
         $id: 'dateDetail',
-        state: {}
+        state: {},
+        data: {}
     });
 
-
+    vm.$watch('data', function(){
+        log('change');
+    });
     return vm;
 });
