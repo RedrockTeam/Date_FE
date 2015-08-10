9c0e96635edad46aa169567accd559b0ac67a016
刘慧芝 <liuhuizhi@liuhuizhideMacBook-Air.local>
2015-08-10 15:24:06 +0800

    修正前面merge错误

--- src/lib/vms/main.js
+++ src/lib/vms/main.js
@@ -1,23 +1,28 @@
 
-define(['avalon', 'jquery','vms/slider', 'vms/topBar','dialog', 'avaFilters','mmState'], function(avalon,$, vmSlider, vmTopBar){
-    var vm = avalon.define({
-        $id: "main",
-        state: "",
-        loadCout: 0,
-        sliderCb: function(){
-            //测试
-            vmSlider['slider']();
-        },
-        moduleLoadCb: function() {    //测试
-            vm.loadCout++;
-            if (vm.loadCout == 4) {   //
-                log('modlues 加载完毕!!!');
-                console.log(vm.loadCout);
-                vmSlider['moduleSlider']();
-                vm['state'] = '';
-
+define(
+    [
+        'avalon', 'jquery', 'vms/slider',
+        'vms/topBar', 'vms/navBar',
+        'vms/filter', 'dialog',
+        'avaFilters', 'mmState'
+    ],
+    function (avalon, $, vmSlider, vmTopBar, vmNavBar, vmFilter) {
+        var vm = avalon.define({
+            $id: "main",
+            state: "",
+            loadCout: 0,
+            sliderCb: function () {
+                //测试
+                vmSlider['slider']();
+            },
+            moduleLoadCb: function () {    //测试
+                if (vm.loadCout > 4) vm.loadCout = 0;
+                vm.loadCout++;
+                if (vm.loadCout == 4) {   //
+                    log('modlues 加载完毕!!!');
+                    vmSlider['moduleSlider']();
+                }
             }
-        }
         });
 
         //滑动依赖
